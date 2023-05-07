44c0c0995cc75d3f66e9bd3ea89c3f1b843c810d
Mariia Pinchak <marypinchak1@gmail.com>
2023-05-07 20:49:44 +0300

    add-changes

--- src/components/hero_slider/index.tsx
+++ src/components/hero_slider/index.tsx
@@ -1,24 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+
+import Swiper from "swiper";
+import "swiper/swiper-bundle.min.css"; 
 import s from "./index.module.scss";
-import Swiper from 'swiper';
-import Image from 'next/image';
-import Slide1 from '../../assets/images/hero-slide1.png';
+import Image from "next/image";
+import Slide1 from "../../assets/images/hero-slide1.png";
 
 const HeroSlider = () => {
-
     useEffect(() => {
-        const swiper = new Swiper('.swiper-container', {
-            // Swiper options here
+        const swiper = new Swiper(".swiper", {
+            loop: true,
+            slidesPerView: 1,
+            pagination: {
+                el: ".swiper-pagination, .swiper_pagination",
+                bulletClass: "swiper-pagination-bullet swiper_pagination_bullet",
+                clickable: true,
+            },
         });
     }, []);
 
     return (
-        <>
-
-            <div className={`swiper-container ${s.hero_slider}`}>
-                <div className={`swiper-wrapper ${s.hero_slider__wrapper}`}>
-                    <div className={`swiper-slide ${s.hero_slider__slide}`}>
-                        <div className={s.hero_slider__image_container}>
+        <section className={s.hero}>
+            <div className={`swiper ${s.hero__slider}`}>
+                <div className={`swiper-wrapper ${s.hero__slider_wrapper}`}>
+                    <div className={`swiper-slide ${s.hero__slider_slide}`}>
+                        <div className={s.hero__slider_image_container}>
                             <Image
                                 src={Slide1}
                                 alt="Picture of the author"
@@ -27,17 +33,45 @@ const HeroSlider = () => {
                                 quality={100}
                             />
 
-                            <div className={s.hero_slider__circle}>
-                                <div className={s.hero_slider__circle_text}>
-                                    Text <span className={s.hero_slider__middle_dot}>&middot;</span>
+                            <div className={s.hero__slider_circle}>
+                                <div className={s.hero__slider_circle_text}>
+                                    <p className={s.hero__slider_circle_text_first}>SHOP</p>
+                                    <p className={s.hero__slider_circle_text_second}>
+                                        CLOTHING <span></span>
+                                    </p>
+                                    <p className={s.hero__slider_circle_text_third}>HERE</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`swiper-slide ${s.hero__slider_slide}`}>
+                        <div className={s.hero__slider_image_container}>
+                            <Image
+                                src={Slide1}
+                                alt="Picture of the author"
+                                layout="fill"
+                                objectFit="cover"
+                                quality={100}
+                            />
+
+                            <div className={s.hero__slider_circle}>
+                                <div className={s.hero__slider_circle_text}>
+                                    <p className={s.hero__slider_circle_text_first}>SHOP</p>
+                                    <p className={s.hero__slider_circle_text_second}>
+                                        CLOTHING <span></span>
+                                    </p>
+                                    <p className={s.hero__slider_circle_text_third}>HERE+</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="swiper-pagination"></div>
+                <div className={`swiper-pagination ${s.swiper_pagination}`}>
+                    <span className={`swiper-pagination-bullet ${s.swiper_pagination_bullet}`}></span>
+                    <span className={`swiper-pagination-bullet swiper-pagination-bullet-active ${s.swiper_pagination_bullet} ${s.swiper_pagination_bullet_active}`}></span>
+                </div>
             </div>
-        </>
+        </section>
     );
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 
+
 // layouts
 import Layout from "@/layouts";
 
 
 export default function App({ Component, pageProps }: AppProps) {
+  
   return (
     <>
       <Layout>
