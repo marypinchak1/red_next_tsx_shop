2719016aa939f2977e476288134f841dd554bf76
Mariia Pinchak <marypinchak1@gmail.com>
2023-05-07 23:07:21 +0300

    add

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from 'next/router';
+import ProductCard from "../products_card";
 import s from "./index.module.scss";
 
 interface Category {
@@ -25,6 +26,7 @@ const Sidebar: React.FC<SidebarProps> = ({ products }) => {
   const router = useRouter();
   const { pathname } = router;
   const [categories, setCategories] = useState<Category[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string>('');
 
   useEffect(() => {
     // Extract unique categories from products data
@@ -39,6 +41,12 @@ const Sidebar: React.FC<SidebarProps> = ({ products }) => {
     setCategories(categoryObjects);
   }, [products]);
 
+  const handleCategoryClick = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredProducts = selectedCategory ? products.filter(product => product.category === selectedCategory) : products;
+
   return (
     <>
       <body>
@@ -49,12 +57,18 @@ const Sidebar: React.FC<SidebarProps> = ({ products }) => {
             <ul>
               {categories.map((category, index) => (
                 <li key={index} className={pathname === category.path ? 'active' : ''}>
-                  <Link href={category.path} className={s.header__menu_link}>{category.name}</Link>
+                  <a className={s.header__menu_link} onClick={() => handleCategoryClick(category.name)}>{category.name}</a>
                 </li>
+                
               ))}
             </ul>
           </nav>
         </section>
+        <section className={s.products}>
+        {filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </section>
       </body>
     </>
   );
